eac8a9ac517749d2e537fa8aab4f01a1dc44e43f
Nicola Guidi <nicola.guidi@me.com>
2015-09-01 16:15:54 +0000

    Add form validation

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,8 @@
 class Quote < ActiveRecord::Base
+  validates :saying, :presence => true,
+                     :length => { :minimum => 3,
+                                  :maximum => 140 }
+  validates :author, :presence => true,
+                     :length => { :minimum => 3,
+                                  :maximum => 50 }
 end
