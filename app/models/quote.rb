f0eaab43b7752d55077b08dcd275f758a9ff22d8
Nicola Guidi <nicola.guidi@me.com>
2015-09-25 00:11:08 +0000

    Adds a unique_tag method and displays initials on index page

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,4 +5,12 @@ class Quote < ActiveRecord::Base
   validates :author, :presence => true,
                      :length => { :minimum => 3,
                                   :maximum => 50 }
+
+  def unique_tag
+    abbr = self.author.split(' ').collect do |sub_string|
+      sub_string[0]
+    end
+
+    abbr.join + "#" + self.id.to_s
+  end
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  # test "the truth" do
+  #   assert true
+  # end
+  test "unique_tag" do
+    quote = Quote.create(:author => 'John McClane', :saying => 'Yippee-ki-yay, motherfucker.')
+    expected = 'JM#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
+    # puts quote.inspect
   end
 end
